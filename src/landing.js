fd3c838d58bc340e77600a2de86047abfc5c68f7
MahdiFakhr <anonymousaxis@gmail.com>
2019-06-05 01:54:31 +0430

    Fix a bug that opens several overlays

--- src/landing.js
+++ src/landing.js
@@ -5,6 +5,9 @@ const path = require("path");
 const url = require("url");
 
 
+
+
+
 // get app title from <title> tag 
 let title = document.getElementById("AppTitle").innerHTML;
 
@@ -13,47 +16,61 @@ document.getElementById("titleShown").innerHTML = title;
 
 // get startTheGame click event for opening overlay form
 const startTheGameBtn = document.getElementById("startTheGameBtn");
+
+
+isFirst = sessionStorage.getItem("IsFirstWindow");
+
+
 startTheGameBtn.addEventListener("click", (event) => {
-    // create new window here
-    let overlayWindow = new RemoteOverlayWindow({
-        // make it fullscreen and transparent so we can put html elements on it!, without creating new Browser window
-        hasShadow: false,
-        show:false,
-        transparent: true,
-        frame: false,
-        fullscreen: true,
-        x: 0,
-        y: 0,
-        width: screen.availWidth,
-        height: screen.availHeight,
-        resizable: false,
-    });
-    // set our window on top of bordless windowed games
-    overlayWindow.setAlwaysOnTop(true);
+    if (isFirst === true || isFirst === null) {
+        isFirst = sessionStorage.setItem("IsFirstWindow", "false")
+        // create new window here
+        overlayWindow = new RemoteOverlayWindow({
+            // make it fullscreen and transparent so we can put html elements on it!, without creating new Browser window
+            hasShadow: false,
+            show: false,
+            transparent: true,
+            frame: false,
+            fullscreen: true,
+            x: 0,
+            y: 0,
+            width: screen.availWidth,
+            height: screen.availHeight,
+            resizable: false,
+        });
 
-    // hide the overlay icon in taskbar
-    overlayWindow.setSkipTaskbar(true);
+        isFirstWindow = true;
 
-    // load overlay.Html
-    // they are in the same path
-    overlayWindow.loadURL(url.format({
-        pathname: path.join(__dirname,"overlay.html"),
-        protocol: "file:",
-        slashes: true,
-    }))
+        console.log(LandingRemote.getGlobal());
 
-    // close event 
-    overlayWindow.on("closed", () => {
-        overlayWindow = null;
-    })
+        // set our window on top of bordless windowed games
+        overlayWindow.setAlwaysOnTop(true);
 
-    // show window when it was rdy
-    overlayWindow.once("ready-to-show", () => {
-        overlayWindow.show();
-        overlayWindow.focus();
-    })
+        // hide the overlay icon in taskbar
+        overlayWindow.setSkipTaskbar(true);
 
-    // after opening new one we have to close landing window
-    let landingRemote = LandingRemote.getCurrentWindow();
-    //landingRemote.close();
+        // load overlay.Html
+        // they are in the same path
+        overlayWindow.loadURL(url.format({
+            pathname: path.join(__dirname, "overlay.html"),
+            protocol: "file:",
+            slashes: true,
+        }))
+
+        // close event 
+        overlayWindow.on("closed", () => {
+            overlayWindow = null;
+        })
+
+        // show window when it was rdy
+        overlayWindow.once("ready-to-show", () => {
+            overlayWindow.show();
+            overlayWindow.focus();
+        })
+
+        // after opening new one we have to close landing window
+        let landingRemote = LandingRemote.getCurrentWindow();
+        // landingRemote.close();
+        // -----------------------------
+    }
 });
