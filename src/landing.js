8062dcc01eb5a7815c26d1632d4b0052ca2a71cf
MahdiFakhr <anonymousaxis@gmail.com>
2019-06-05 00:12:17 +0430

    add MainWindow Browser

---
+++ src/landing.js
@@ -0,0 +1,5 @@
+// get app title from <title> tag 
+let title = document.getElementById("AppTitle").innerHTML;
+
+// update window browser title with App title
+document.getElementById("titleShown").innerHTML = title;
