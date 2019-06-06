68a930bdcd0a4925de87bea1f908bec71e3cf109
MahdiFakhr <anonymousaxis@gmail.com>
2019-06-07 00:40:36 +0430

    add dynamic text to radio subtitle

--- src/overlay.js
+++ src/overlay.js
@@ -1,6 +1,68 @@
-// handle the backend codes
+let radioSubtitle = document.getElementById("Arty_type_radio_subtitle");
+
+// use global change method to track everything
+document.addEventListener("change",() => {
+    // get value of selected radio button
+    let radioTitle = document.querySelector('input[name="arty_type"]:checked').value; 
+    // send value to change the subtitle method
+    radioSubtitle.innerText = radioTitle;
+
+
+    // show result coords
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // error list
 const errorList = {
-    
+    emptyFields: "Fields can not be empty",
+    closeTarget: "Target is too close to artillery ",
+    sameCoords: "Target and Friendly distance and azimuth must not be the same.",
+    zeroCoords: "Target and Friendly distance must not be both 0.",
+}
+
+// Arty ranges
+const artilleryRanges = {
+    Mortar: {
+        MinRange: 45,
+        MaxRange: 65
+    },
+    Howitzer: {
+        MinRange: 75,
+        MaxRange: 150
+    },
+    Gunboat: {
+        MinRange: 50,
+        MaxRange: 100
+    },
+    Fieldartillery: {
+        MinRange: 75,
+        MaxRange: 150
+    }
 }
