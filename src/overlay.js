915255d02f3b19f8a15521d7d2e31fc83881a6da
MahdiFakhr <anonymousaxis@gmail.com>
2019-06-06 16:22:02 +0430

    add animation to overlay form

--- src/overlay.js
+++ src/overlay.js
@@ -1,3 +1,53 @@
+let openOverlay = document.getElementById("openOverlay");
+let overlayBox = document.getElementById("overlayBox");
+
+
+
+openOverlay.addEventListener("click",() => {
+    // 1. open the overlay container
+    overlayBox.style.width = "360px";
+    overlayBox.style.height = "686px";
+    overlayBox.style.borderRadius = "5px";
+    // 2. add tranistion class to overlay Opening
+    overlayBox.classList.add("overlay_Animation");
+    // 3. display none for openOverlay
+    openOverlay.classList.add("display_none");
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Arty types Dic
