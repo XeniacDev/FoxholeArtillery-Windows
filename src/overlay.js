a2a820c48e91d49b7549f0d55dc565627d8ac1e8
MahdiFakhr <anonymousaxis@gmail.com>
2019-06-06 21:11:59 +0430

    change margin top of entire form after clicking on arty ico n

--- src/overlay.js
+++ src/overlay.js
@@ -1,6 +1,7 @@
 let openOverlay = document.getElementById("openOverlay");
 let overlayBox = document.getElementById("overlayBox");
 let overlayContent = document.getElementById("overlay_content");
+let overlayWallpaper = document.getElementById("overlay_wallpaper");
 
 overlayContent.style.opacity = 0;
 
@@ -9,6 +10,8 @@ openOverlay.addEventListener("click",() => {
     overlayBox.style.width = "360px";
     overlayBox.style.height = "686px";
     overlayBox.style.borderRadius = "5px";
+    // .. set new marginTOp for better view 
+    overlayBox.style.marginTop = "85px";
     // 2. add tranistion class to overlay Opening
     overlayBox.classList.add("overlay_Animation");
     // 3. display none for openOverlay
@@ -16,12 +19,11 @@ openOverlay.addEventListener("click",() => {
     //4 show content
     // .. unfade 
     overlayContent.style.opacity = 1;
+    // .. remove display class from wallpaper
+    overlayWallpaper.classList.remove("display_none");
     // .. moving animation
     //overlayContent.style.paddingTop = 0;
 });
-
-
-
 
 
 
